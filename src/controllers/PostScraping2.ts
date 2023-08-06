c95ab9a05c9c850d0a442d1112056ed0d6af0651
Laudier2 <trabalhandooline2019@hotmail.com>
2023-08-05 22:35:06 -0300

    update

---
+++ src/controllers/PostScraping2.ts
@@ -0,0 +1,85 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import * as puppeteer from 'puppeteer';
+
+const url = "https://multicanais.fans/aovivohd/assistir-esportes-online/"
+
+const prisma = new PrismaClient()
+
+export class ControllerPost2 {
+    async handle(request: Request, response: Response) {
+        const { title, imagem, video } = request.body;
+
+        const brawser = await puppeteer.launch({
+            //ignoreDefaultArgs: ['--disable-extensions'],
+            //args: ['--no-sandbox', '--disable-setuid-sandbox']
+            headless: false,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
+        })
+        const page = await brawser.newPage()
+
+        await page.goto(url, { timeout: 0 })
+
+        const links = await page.$$eval('.entry-title > a', el => el.map(link => link.href))
+        const imgs = await page.$$eval('.entry-image > a > img', imgs => imgs.map(img => img.getAttribute('src')));
+        const title2 = await page.$$eval(".entry-title", (elements) => { return elements.map((el) => el.textContent.trim()) })
+
+        if (
+            typeof title2 === 'number' ||
+            typeof imgs === 'number' ||
+            typeof links === 'number'
+        ) {
+            return response.status(500).json({
+                msg: `Algum campo estar em número! 
+          Lembre-se que, todos os campos tem estar em string ok!
+          Ou o id esta errado, verifique novamente
+          `
+            })
+        }
+
+        if (
+            typeof title2 === 'undefined' ||
+            typeof imgs === 'undefined' ||
+            typeof links === 'undefined'
+        ) {
+            return response.status(500).json({
+                msg: `Algum campo esta faltando! Verifique novamente!
+          Ou o id esta errado, verifique novamente`
+            })
+        }
+
+        const data2 = {
+            title: title2,
+            link2: links,
+            img2: imgs
+        }
+
+
+        try {
+            for (let i = 0; i < 20; i++) {
+                const img = data2.img2[i]
+                const t = data2.title[i]
+                const v = data2.link2[i]
+
+
+                const PostFut = await prisma.listaFut.create({
+
+                    data: {
+                        title: `${t}`,
+                        imagem: `${img}`,
+                        video: `${v}`
+                    }
+                })
+            }
+        } catch (error) {
+
+            await brawser.close()
+
+            return response.json({ msg: error })
+        }
+
+        await brawser.close()
+
+    }
+
+}
--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { ControllerFind } from "../controllers/FindScraping";
 import { ControllerPost } from "../controllers/PostScraping";
+import { ControllerPost2 } from "../controllers/PostScraping2";
 import { controllerDelete, controllerDeleteTodos } from "../controllers/DelScraping";
 
 
@@ -9,6 +10,7 @@ const router = Router();//
 /* ============================== OBJETO CRIADOS PARA ROTAS ====================== */
 const createFind = new ControllerFind();
 const createPost = new ControllerPost();
+const createPost2 = new ControllerPost2();
 const createDell = new controllerDelete();
 const createDellTodos = new controllerDeleteTodos();
 
@@ -18,5 +20,6 @@ router.get("/", createFind.handle);
 router.delete("/del/:id", createDell.handle);
 router.delete("/del", createDellTodos.handle);
 router.post("/api", createPost.handle);
+router.post("/api2", createPost2.handle);
 
 export { router };
